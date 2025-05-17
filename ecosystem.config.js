635207911bc4fad4a9cb1e57b2960a48a6643bb9
Eugene Koshelev <renderlife@mail.ru>
2025-05-17 23:21:53 +0300

    Правка старта pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,21 +3,13 @@ module.exports = {
         {
             name: "logger-old-node-backend",
             script: "app.js",
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: "1G",
             env: {
-                NODE_ENV: 'development',
+                NODE_ENV: "development",
             },
             env_production: {
                 NODE_ENV: "production",
                 PORT: 3003,
             },
-            log_date_format: "YYYY-MM-DD HH:mm:ss",
-            error_file: "./logs/pm2-error.log",
-            out_file: "./logs/pm2-out.log",
-            combine_logs: true,
         },
     ],
 };
